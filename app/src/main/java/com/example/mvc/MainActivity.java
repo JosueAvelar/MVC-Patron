8c3a3b5ea995ca0ae60784a70af332c3af4cff0a
Dayanna <dzuniga145@gmail.com>
2022-04-02 23:19:21 -0600

    Cambio de login y el main

--- app/src/main/java/com/example/mvc/MainActivity.java
+++ app/src/main/java/com/example/mvc/MainActivity.java
@@ -8,13 +8,39 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.mvc.controller.ILoginController;
+import com.example.mvc.controller.LoginController;
+import com.example.mvc.view.ILoginView;
 
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ILoginView {
+    EditText email,password;
+    Button loginb;
+    ILoginController loginPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText)findViewById(R.id.password);
+
+        loginb = (Button) findViewById(R.id.loginb);
+        loginPresenter = new LoginController(this);
+
+        loginb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginPresenter.OnLogin(email.getText().toString().trim(),password.getText().toString().trim());
+            }
+        });
+    }
+
+    public void OnLoginSuccess(String message) {
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+    }
+
+    public void OnLoginError(String message) {
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/mvc/view/ILoginView.java
+++ app/src/main/java/com/example/mvc/view/ILoginView.java
@@ -1,4 +1,6 @@
 package com.example.mvc.view;
 
 public interface ILoginView {
+    void OnLoginSuccess(String message);
+    void OnLoginError(String message);
 }
