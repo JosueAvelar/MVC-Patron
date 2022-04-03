6a452f66a6556355aace0c7a1d07d64b88bb3fdc
JosueAvelar <avelarjosue1@gmail.com>
2022-04-02 22:31:02 -0600

    Primera parte del MVC

--- app/src/main/java/com/example/mvc/MainActivity.java
+++ app/src/main/java/com/example/mvc/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.mvc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/mvc/controller/ILoginController.java
@@ -0,0 +1,5 @@
+package com.example.mvc.controller;
+
+public interface ILoginController {
+    void OnLogin(String email,String Password);
+}
